7a9f726862b8eedec4f786c89b09e96716534d81
rahul8073 <rahulkumarsingh1473@gmail.com>
2024-05-29 23:21:13 +0530

    fix

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -15,7 +15,7 @@ import apple from '../assets/apple.png';
 import {useNavigation} from '@react-navigation/native';
 import SelectDropdown from 'react-native-select-dropdown';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
-
+import cameraicon from '../assets/cameraIcon.png';
 const {width} = Dimensions.get('window');
 
 export default function SignUp() {
@@ -45,6 +45,14 @@ export default function SignUp() {
 
   const [imageUri, setImageUri] = useState(null);
 
+  const scheduling = [
+    '8:00am - 10:00am',
+    '10:00am - 01:00pm',
+    '1:00pm - 4:00pm',
+    '4:00pm - 7:00pm',
+    '7:00pm - 10:00pm',
+  ];
+
   const handleImagePicker = () => {
     launchImageLibrary({mediaType: 'photo'}, response => {
       if (response.didCancel) {
@@ -56,6 +64,7 @@ export default function SignUp() {
       }
     });
   };
+  console.log(imageUri, 'kkkkkkkkkkkkkkk');
   return (
     <View className="flex flex-1 bg-white">
       <ScrollView
@@ -242,29 +251,30 @@ export default function SignUp() {
               <View className="mt-16">
                 <Text className="text-4xl text-black">Verification</Text>
                 <Text>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Officia mollitia incidunt, blanditiis soluta praesentium iste
-                  dolor doloremque nulla ipsam reiciendis!
+                  Attach proof of Department of Agriculture registrations i.e.
+                  Florida Fresh, USDA Approved, USDA Organic
                 </Text>
               </View>
               <View className="mt-12">
-                <View>
-                  <Text >
-                    Attach proof of Department of Agriculture registrations i.e.
-                    Florida Fresh, USDA Approved, USDA Organic
-                  </Text>
+                <View className="flex flex-row justify-between">
+                  <View className="">
+                    <Text className="text-orange-700">
+                      Attach proof of of Registration
+                    </Text>
+                  </View>
                   <TouchableOpacity
-                    onPress={handleImagePicker}>
-                    <Image
-                      source={require('./path-to-camera-icon.png')}
-                    />
+                    onPress={handleImagePicker}
+                    className="flex bg-orange-500 flex-row items-center justify-center p-1 rounded-lg">
+                    <Image source={cameraicon} className="h-5 w-5" />
                   </TouchableOpacity>
+                </View>
+                <View className="flex flex-row justify-center my-4">
                   {imageUri && (
                     <Image
                       source={{uri: imageUri}}
+                      style={{width: width, height: 400}}
                     />
                   )}
-                 
                 </View>
 
                 <View className="my-5 flex flex-row justify-between bottom-0 ">
@@ -289,62 +299,45 @@ export default function SignUp() {
             <Text className="text-gray-600">Farmer Eats</Text>
             <View>
               <View className="mt-16">
-                <Text className="text-4xl text-black">Welcome!</Text>
-                <View className="flex flex-row gap-x-8 mt-3">
-                  <TouchableOpacity>
-                    <Image source={google} className="w-8 h-8" />
+                <Text className="text-4xl text-black">Business Hours</Text>
+                <Text className="text-justify">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Doloremque, culpa!
+                </Text>
+              </View>
+
+              <View className="mt-16">
+                {/* days */}
+                <View className="flex flex-row  gap-x-4 w-full my-3 ">
+                  <TouchableOpacity className="bg-gray-200 px-2 text-black rounded-lg">
+                    <Text>M</Text>
                   </TouchableOpacity>
-                  <TouchableOpacity>
-                    <Image source={apple} className="w-8 h-8" />
+                  <TouchableOpacity className="bg-gray-200 px-2 text-black rounded-lg">
+                    <Text>T</Text>
                   </TouchableOpacity>
-                  <TouchableOpacity>
-                    <Image source={fb} className="w-8 h-8" />
+                  <TouchableOpacity className="bg-orange-500 px-2 text-black rounded-lg">
+                    <Text>W</Text>
                   </TouchableOpacity>
+                  <TouchableOpacity className="bg-gray-200 px-2 text-black rounded-lg">
+                    <Text>T</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity className="bg-gray-200 px-2 text-black rounded-lg">
+                    <Text>F</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity className="bg-gray-200 px-2 text-black rounded-lg">
+                    <Text>S</Text>
+                  </TouchableOpacity>
+                </View>
+                {/* scheduling */}
+                <View className="flex flex-row justify-between flex-wrap">
+                  {scheduling.map((item, index) => (
+                    <TouchableOpacity className=" rounded-lg bg-orange-700 py-2 px-2 m-2">
+                      <Text>{item}</Text>
+                    </TouchableOpacity>
+                  ))}
                 </View>
               </View>
               <View className="mt-12">
-                <View className="flex flex-row gap-x-2 bg-red-100 rounded-xl px-3 my-2">
-                  <View className="flex flex-row items-center">
-                    <Icon name="user" size={18} color="black" />
-                  </View>
-                  <TextInput placeholder="Full name" className="w-full" />
-                </View>
-                <View className="flex flex-row gap-x-2 bg-red-100 rounded-xl px-3 my-2">
-                  <View className="flex flex-row items-center">
-                    <Icon name="at" size={18} color="black" />
-                  </View>
-                  <TextInput placeholder="Email address" className="w-full" />
-                </View>
-                <View className="flex flex-row bg-red-100 rounded-xl px-3 my-2 gap-x-2">
-                  <View className="flex flex-row items-center">
-                    <Icon name="phone" size={18} color="black" />
-                  </View>
-                  <TextInput
-                    placeholder="Phone number"
-                    className="w-full"
-                    keyboardType="numeric"
-                  />
-                </View>
-                <View className="flex flex-row gap-x-2 overflow-hidden bg-red-100 rounded-xl px-3 my-2 justify-between">
-                  <View className="flex flex-row items-center gap-1">
-                    <Icon name="lock" size={20} color="black" />
-                    <TextInput
-                      placeholder="Password"
-                      className=""
-                      secureTextEntry
-                    />
-                  </View>
-                </View>
-                <View className="flex flex-row gap-x-2 overflow-hidden bg-red-100 rounded-xl px-3 my-2 justify-between">
-                  <View className="flex flex-row items-center gap-1">
-                    <Icon name="lock" size={20} color="black" />
-                    <TextInput
-                      placeholder="Re-enter Password"
-                      className=""
-                      secureTextEntry
-                    />
-                  </View>
-                </View>
                 <View className="my-5 flex flex-row justify-between">
                   <TouchableOpacity
                     onPress={handleBack}
@@ -366,148 +359,22 @@ export default function SignUp() {
           <View style={{width}} className="p-4">
             <Text className="text-gray-600">Farmer Eats</Text>
             <View>
-              <View className="mt-16">
-                <Text className="text-4xl text-black">Farm Info</Text>
-                <View className="flex flex-row gap-x-8 mt-3">
-                  <TouchableOpacity>
-                    <Image source={google} className="w-8 h-8" />
-                  </TouchableOpacity>
-                  <TouchableOpacity>
-                    <Image source={apple} className="w-8 h-8" />
-                  </TouchableOpacity>
-                  <TouchableOpacity>
-                    <Image source={fb} className="w-8 h-8" />
-                  </TouchableOpacity>
-                </View>
-              </View>
               <View className="mt-12">
-                <View className="flex flex-row gap-x-2 bg-red-100 rounded-xl px-3 my-2">
-                  <View className="flex flex-row items-center">
-                    <Icon name="user" size={18} color="black" />
-                  </View>
-                  <TextInput placeholder="Full name" className="w-full" />
-                </View>
-                <View className="flex flex-row gap-x-2 bg-red-100 rounded-xl px-3 my-2">
-                  <View className="flex flex-row items-center">
-                    <Icon name="at" size={18} color="black" />
-                  </View>
-                  <TextInput placeholder="Email address" className="w-full" />
-                </View>
-                <View className="flex flex-row bg-red-100 rounded-xl px-3 my-2 gap-x-2">
-                  <View className="flex flex-row items-center">
-                    <Icon name="phone" size={18} color="black" />
-                  </View>
-                  <TextInput
-                    placeholder="Phone number"
-                    className="w-full"
-                    keyboardType="numeric"
-                  />
-                </View>
-                <View className="flex flex-row gap-x-2 overflow-hidden bg-red-100 rounded-xl px-3 my-2 justify-between">
-                  <View className="flex flex-row items-center gap-1">
-                    <Icon name="lock" size={20} color="black" />
-                    <TextInput
-                      placeholder="Password"
-                      className=""
-                      secureTextEntry
-                    />
-                  </View>
-                </View>
-                <View className="flex flex-row gap-x-2 overflow-hidden bg-red-100 rounded-xl px-3 my-2 justify-between">
-                  <View className="flex flex-row items-center gap-1">
-                    <Icon name="lock" size={20} color="black" />
-                    <TextInput
-                      placeholder="Re-enter Password"
-                      className=""
-                      secureTextEntry
-                    />
-                  </View>
-                </View>
-                <View className="my-5 flex flex-row justify-between">
-                  <TouchableOpacity
-                    onPress={handleBack}
-                    className="flex flex-row items-center">
-                    <Icon name="long-arrow-left" size={18} color="black" />
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    onPress={handleNext}
-                    className="bg-orange-700 py-4 px-16 rounded-full">
-                    <Text className="text-center text-white text-xl font-bold">
-                      Continue
-                    </Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-            </View>
-          </View>
-          {/* sixth page */}
-          <View style={{width}} className="p-4">
-            <Text className="text-gray-600">Farmer Eats</Text>
-            <View>
-              <View className="mt-16">
-                <Text className="text-4xl text-black">Welcome!</Text>
-                <View className="flex flex-row gap-x-8 mt-3">
-                  <TouchableOpacity>
-                    <Image source={google} className="w-8 h-8" />
-                  </TouchableOpacity>
-                  <TouchableOpacity>
-                    <Image source={apple} className="w-8 h-8" />
-                  </TouchableOpacity>
-                  <TouchableOpacity>
-                    <Image source={fb} className="w-8 h-8" />
-                  </TouchableOpacity>
-                </View>
-              </View>
-              <View className="mt-12">
-                <View className="flex flex-row gap-x-2 bg-red-100 rounded-xl px-3 my-2">
-                  <View className="flex flex-row items-center">
-                    <Icon name="user" size={18} color="black" />
-                  </View>
-                  <TextInput placeholder="Full name" className="w-full" />
-                </View>
-                <View className="flex flex-row gap-x-2 bg-red-100 rounded-xl px-3 my-2">
-                  <View className="flex flex-row items-center">
-                    <Icon name="at" size={18} color="black" />
-                  </View>
-                  <TextInput placeholder="Email address" className="w-full" />
-                </View>
-                <View className="flex flex-row bg-red-100 rounded-xl px-3 my-2 gap-x-2">
-                  <View className="flex flex-row items-center">
-                    <Icon name="phone" size={18} color="black" />
-                  </View>
-                  <TextInput
-                    placeholder="Phone number"
-                    className="w-full"
-                    keyboardType="numeric"
-                  />
-                </View>
-                <View className="flex flex-row gap-x-2 overflow-hidden bg-red-100 rounded-xl px-3 my-2 justify-between">
-                  <View className="flex flex-row items-center gap-1">
-                    <Icon name="lock" size={20} color="black" />
-                    <TextInput
-                      placeholder="Password"
-                      className=""
-                      secureTextEntry
-                    />
-                  </View>
-                </View>
-                <View className="flex flex-row gap-x-2 overflow-hidden bg-red-100 rounded-xl px-3 my-2 justify-between">
-                  <View className="flex flex-row items-center gap-1">
-                    <Icon name="lock" size={20} color="black" />
-                    <TextInput
-                      placeholder="Re-enter Password"
-                      className=""
-                      secureTextEntry
-                    />
-                  </View>
-                </View>
-                <View className="my-5 flex flex-row justify-between">
+                <Text className="text-center font-bold text-black text-2xl">
+                  You're All done
+                </Text>
+                <Text className="text-justify">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia
+                  beatae saepe repudiandae fuga ea ipsum perferendis obcaecati
+                  iusto, voluptatum natus.
+                </Text>
+                <View className="my-5 flex flex-row justify-center">
                   {/* <TouchableOpacity onPress={handleBack} className="flex flex-row items-center">
                     <Icon name="long-arrow-left" size={18} color="black" />
                   </TouchableOpacity> */}
                   <TouchableOpacity
                     onPress={() => navigation.navigate('Login')}
-                    className="bg-orange-700 py-4 px-16 rounded-full">
+                    className="bg-orange-700 py-4 px-16 rounded-full w-full">
                     <Text className="text-center text-white text-xl font-bold">
                       Got it!
                     </Text>
